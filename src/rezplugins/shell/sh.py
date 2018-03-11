9ccc515e658a85aef9bb96ed0f46800d47fe0b70
skral <git@sebastiankral.de>
2018-03-10 22:50:34 -0500

    Update for further sh testing

--- src/rezplugins/shell/sh.py
+++ src/rezplugins/shell/sh.py
@@ -92,16 +92,12 @@ class SH(UnixShell):
 
     def _bind_interactive_rez(self):
         if config.set_prompt and self.settings.prompt:
-            self._addline('echo =====PROMPT======')
-            self._addline('echo $PS1')
             self._addline('if [ -z "$REZ_STORED_PROMPT" ]; then export REZ_STORED_PROMPT=$PS1; fi')
             if config.prefix_prompt:
                 cmd = 'export PS1="%s $REZ_STORED_PROMPT"'
             else:
                 cmd = 'export PS1="$REZ_STORED_PROMPT%s "'
             self._addline(cmd % "\[\e[1m\]$REZ_ENV_PROMPT\[\e[0m\]")
-            self._addline('echo =====PROMPT2======')
-            self._addline('echo $REZ_ENV_PROMPT')
 
     def setenv(self, key, value):
         value = self.escape_string(value)
